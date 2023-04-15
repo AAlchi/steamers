958f24f6ee62c0c935881915fd74036dbbd453c8
Ali Ibrahim <alialchi07@gmail.com>
2023-04-15 10:28:00 -0500

    New Change

--- backend/routes/main.js
+++ backend/routes/main.js
@@ -1,7 +1,6 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-console.log(res)
+router.get("/", async (req, res, next) => {
+  console.log(res);
 });
-

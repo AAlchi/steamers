3d59f70a90f5663ebdfee65b97e59cd679f73a2b
Ali Ibrahim <alialchi07@gmail.com>
2023-04-15 14:05:43 -0500

    ok

--- backend/routes/main.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const fun = require('../[id]')
-
-router.get("/", async (req, res, next) => {
-  fun.getServerSideVolunteers(req);
-});
-
-module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,38 @@
 const express = require("express");
-const cors = require('cors')
-
-const routes = require('./routes/main')
 
 const app = express();
 
-app.options("*", cors());
+const dotenv = require("dotenv");
 
-app.use('/', routes)
+dotenv.config();
 
-app.listen(8080);
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+// using Twilio SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const msg = {
+  to: "alialchi07@gmail.com", // Change to your recipient
+  from: "aalchikhibrahim6522@stu.d214.org", // Change to your verified sender
+  subject: "Sending with SendGrid is Fun",
+  text: "and easy to do anywhere, even with Node.js",
+  html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+};
+sgMail
+  .send(msg)
+  .then(() => {
+    console.log("Email sent");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log("Running on port 5000");
+});
